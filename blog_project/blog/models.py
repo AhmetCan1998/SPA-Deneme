2e5d0859dded8a18da50e97229692b9110a67d5d
Kenan Gülol <kenan.gulol1@protonmail.com>
2021-01-18 00:47:12 +0300

    first commit

---
+++ blog_project/blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+
+
+class User(models.Model):
+    id = models.CharField(primary_key=True, max_length=20, db_column='GUID')
+    name = models.CharField(max_length=30, db_column='NAME', null=False)
+    surname = models.CharField(max_length=30, null=False)
+    mail = models.EmailField(db_column='MAIL', null=False)
+    score = models.FloatField(db_column='USER_SCORE')
+
+    class Meta:
+        db_table = "USER"
+
+
+class Message(models.Model):
+    message_id = models.CharField(primary_key=True,
+                                  max_length=20,
+                                  db_column='MESID')
+    context = models.CharField(max_length=1000,
+                               db_column='CONTEXT',
+                               null=False)
+    sendtime = models.DateTimeField(db_column='SENDTIME', default=timezone.now)
+    sender = models.ForeignKey(User,
+                               db_column='SENDERID',
+                               null=False,
+                               on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "MESSAGE"
